29660101655cc32f09f95546330632e2be3d12f9
gaon000 <gaon2304@gmail.com>
2019-07-18 02:09:30 +0900

    Create server.py

--- server.py
+++ server.py
@@ -16,6 +16,30 @@ app = Flask(__name__)
 api = Api(app)
 
 class signUp(Resource):
-    def get(self)
-        return {'code': "code"}
+    def get(self):
+        code = random_string(6)
+        return {'code': code}
+    
+
+class bet(Resource):
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument("code", type = str, required = True)
+        parser.add_argument("bet")
+        args = parser.args()
+
+class auth(Resource):
+    def post(self):
+        pass
+
+class history(Resource):
+    def get(self):
+        return collection.distinct("code")[0]['history']
+
+class bet2(Resource):#genius
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument("bet", required = True)
+        args = parser.args()
+        return args['bet']
 
